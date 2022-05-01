c115f6579db3a8d9e95da56a2c19280f1230f4ed
Tiju John (TJ) <tjoh@simcorp.com>
2022-05-01 16:51:18 +0200

    something working

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 
 async function main() {
   const app = express.default();
-  
+
   var config = {
     port:3000
   }
@@ -10,6 +10,26 @@ async function main() {
   app.listen(config.port, () => {
     console.log(`Server is listening on port ${config.port}`);
   });
+
+  app.get("/balance", (req, res) => {
+    const balance = [
+      {
+        id: 1,
+        name: "hammer",
+      },
+      {
+        id: 2,
+        name: "screwdriver",
+      },
+      ,
+      {
+        id: 3,
+        name: "wrench",
+      },
+    ];
+ 
+   res.json(balance);
+ });
 
   // graceful shutdown
   process.on('SIGTERM', () => {
