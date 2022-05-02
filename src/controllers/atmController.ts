16e6a8eb12b58ef302fcfe07abc4f5fff8d87e48
Tiju John (TJ) <tjoh@simcorp.com>
2022-05-03 00:44:44 +0200

    negative not allowed

--- src/controllers/atmController.ts
+++ src/controllers/atmController.ts
@@ -9,11 +9,11 @@ const atmManager = new AtmService();
 atmController.post("/withdraw", (req, res) => {
   console.dir(req);
   var amountJson: RequestBody = req.body;
-  if (amountJson?.amount !== undefined && amountJson.amount % 1 === 0) {
+  if (amountJson?.amount !== undefined && amountJson.amount % 1 === 0 && amountJson.amount > 0) {
     res.json(atmManager.getInstance().withDraw(amountJson.amount));
   }
   else {
-    res.statusMessage = "Bad Request. Please specify an integer amount.";
+    res.statusMessage = "Bad Request. Please specify a positive integer amount.";
     res.status(400).end();
   }
 });
