b2853cb12087e2b82df584793ec216a2b05b53f2
Tiju John <tijujohn83@gmail.com>
2022-05-17 14:52:08 +0530

    refactor

--- src/controllers/atmController.ts
+++ src/controllers/atmController.ts
@@ -1,16 +1,16 @@
 import * as express from 'express';
 import { RequestBody } from '../models/requestBody';
-import { AtmService } from '../services/atmService';
+import { AtmManager } from '../services/atmManager';
 
 export const atmController = express.Router();
 
-const atmManager = new AtmService();
+const atmManager = new AtmManager();
 
 atmController.post('/withdraw', (req, res) => {
   console.dir(req);
   const amountJson: RequestBody = req.body;
   if (amountJson?.amount !== undefined && amountJson.amount % 1 === 0 && amountJson.amount > 0) {
-    res.json(atmManager.getInstance().withDraw(amountJson.amount));
+    res.json(atmManager.withDraw(amountJson.amount));
   } else {
     res.statusMessage = 'Bad Request. Please specify a positive integer amount.';
     res.status(400).end();
@@ -18,9 +18,13 @@ atmController.post('/withdraw', (req, res) => {
 });
 
 atmController.get('/balances', (req, res) => {
-  res.json(atmManager.getInstance().getBalances());
+  res.json(atmManager.getBalances());
 });
 
 atmController.get('/balanceAmount', (req, res) => {
-  res.json(atmManager.getInstance().getBalanceValue());
+  res.json(atmManager.getBalanceValue());
+});
+
+atmController.post('/refill', (req, res) => {
+  res.json(atmManager.refill());
 });
--- src/services/atmManager.ts
+++ src/services/atmManager.ts
@@ -7,23 +7,37 @@ import { RandomRefill } from '../strategy/refill/randomRefill';
 import { Normalized } from '../strategy/withdraw/normalized';
 import { OrderByDenominationValueDesc } from '../strategy/withdraw/orderByDenominationValueDesc';
 import { OrderByTotalValueDesc } from '../strategy/withdraw/orderByTotalValueDesc';
+import { WithdrawItems } from '../models/atm/withdrawItems';
+import { WithdrawStatus } from '../models/enums';
 
-export class AtmService {
+export class AtmManager implements AtmInterface {
     private static Instance: Atm;
 
     constructor() {
-        if (AtmService.Instance === undefined) {
+        if (AtmManager.Instance === undefined) {
             // no DI because of time constraints
             const withdrawStratety = new OrderByDenominationValueDesc(); // available: Normalized | OrderByDenominationValueDesc | OrderByTotalValueDesc
             const refillStrategy = new MaxRefill(); // available: RandomRefill | MaxRefill | EqualAmountsRefill
 
-            AtmService.Instance = new Atm(withdrawStratety, refillStrategy, new FileLogger());
-            AtmService.Instance.refill();
+            AtmManager.Instance = new Atm(withdrawStratety, refillStrategy, new FileLogger());
+            AtmManager.Instance.refill();
         }
     }
 
-    public getInstance(): AtmInterface {
-        return AtmService.Instance;
+    public refill(): void {
+        AtmManager.Instance.refill();
+    }
+
+    public getBalances(): WithdrawItems {
+        return AtmManager.Instance.getBalances();
+    }
+
+    public getBalanceValue(): number {
+        return AtmManager.Instance.getBalanceValue();
+    }
+
+    public withDraw(requestedAmount: number): { status: WithdrawStatus; dispensed?: WithdrawItems; } {
+        return AtmManager.Instance.withDraw(requestedAmount);
     }
 
 }
